65eac7ead268c726caa72f54b638d3ed379a0e46
abdulrahman-haroon <abdulrahman.haroonr@gmail.com>
2020-06-08 23:01:53 +0500

    Code

--- ST-Assignment-4-5/src/st/assignment/pkg4/pkg5/STAssignment45.java
+++ ST-Assignment-4-5/src/st/assignment/pkg4/pkg5/STAssignment45.java
@@ -1,21 +1,68 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package st.assignment.pkg4.pkg5;
 
 /**
  *
- * @author abdul
+ * @author abdulrahman
  */
 public class STAssignment45 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void logChecker(int kill,int death,int assist, int hour, int minute, int second )
+    {
+        int flag1=0, flag2=0;
+        if(kill > 15 && death <= 6 && assist >= 8)
+        {
+            flag1=1;
+        }
+        if(hour>12 && minute>=0 && second >=0)
+        {
+            flag2=1;
+        }
+        if(flag1==1 && flag2==1)
+        {
+            System.out.println("CONGRATULATION SUMMONER YOU HAVE ACHIEVED DIAMOND RANK!!! ");
+        }
+        else
+        {
+            System.out.println("Sorry Summoner, you weren't promoted to Diamond Rank.");
+        }
+        
     }
-    
+    public static void main(String[] args) {
+        int kill=16;
+        int death=3;
+        int assist=11;
+        int hour=13;
+        int minute=25;
+        int second=50;
+        int flag1=0,flag2=0;
+        System.out.println("24 hours formate");
+        System.out.println("KILLS | DEATH | ASSIST");
+        System.out.println("");
+        System.out.print("Current Rank of Player:");
+        System.out.println(" GOLD");
+        System.out.println("");
+        System.out.println("Log Checking . . . . . ");
+        System.out.println("");
+        System.out.println("Log Checked Successfully!!");
+        System.out.println("");
+        
+        if(kill>15 && kill <=50 || death>=0 && death <=6 || assist>=8 && assist <=30)
+        {
+            flag1=1;
+        }
+        if(hour>=1 && kill <=24 || minute>=0 && minute <= 60 || second>=0 && second <=60)
+        {
+            flag2=1;
+        }
+        if(flag1==1 && flag2==1)
+        {
+            logChecker(kill,death,assist,hour,minute,second);
+        }
+        else
+        {
+            System.out.println("Better luck next time summoner.");
+        }
+        System.out.println("");
+    }
 }
